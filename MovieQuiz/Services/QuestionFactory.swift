3455850ff2d84d57949fb5daf6825b9f5fd3c5d8
m.kozhukhov <neogenka@gmail.com>
2025-04-15 16:46:12 +0300

    Первый рабочий билд

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 final class QuestionFactory: QuestionFactoryProtocol {
-    weak var delegate: QuestionFactoryDelegate?
+    private let moviesLoader: MoviesLoading
+    private weak var delegate: QuestionFactoryDelegate?
+
+    init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
+        self.moviesLoader = moviesLoader
+        self.delegate = delegate
+    }
     
+    private var movies: [MostPopularMovie] = []
     private var usedQuestionsIndex = [Int]()
-    private let questions: [QuizQuestion] = [
+/*    private let questions: [QuizQuestion] = [
         QuizQuestion(
             image: "The Godfather",
             correctAnswer: true),
@@ -42,24 +49,50 @@ final class QuestionFactory: QuestionFactoryProtocol {
         QuizQuestion(
             image: "Vivarium",
             correctAnswer: false)
-    ]
+    ] */
 
-    func requestNextQuestion() {
-        guard var index = (0..<questions.count).randomElement() else {
-            delegate?.didReceiveNextQuestion(question: nil)
-            return
-        }
-        
-        if questions.count != 0 {
-            while usedQuestionsIndex.contains(index) {
-                index = (0..<questions.count).randomElement()!
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let mostPopularMovies):
+                self.movies = mostPopularMovies.items // сохраняем фильм в нашу новую переменную
+                self.delegate?.didLoadDataFromServer() // сообщаем, что данные загрузились
+            case .failure(let error):
+                self.delegate?.didFailToLoadData(with: error) // сообщаем об ошибке нашему MovieQuizViewController
             }
         }
-        
-        usedQuestionsIndex.append(index)
-        
-        let question = questions[safe: index]
-        delegate?.didReceiveNextQuestion(question: question)
+    }
+    
+    func requestNextQuestion() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            let index = (0..<self.movies.count).randomElement() ?? 0
+            
+            guard let movie = self.movies[safe: index] else { return }
+            
+            var imageData = Data()
+           
+           do {
+               imageData = try Data(contentsOf: movie.resizedImageURL)
+            } catch {
+                print("Failed to load image")
+            }
+            
+            let rating = Float(movie.rating) ?? 0
+            
+            let text = "Рейтинг этого фильма больше чем 7?"
+            let correctAnswer = rating > 7
+            
+            let question = QuizQuestion(image: imageData,
+                                         text: text,
+                                         correctAnswer: correctAnswer)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.delegate?.didReceiveNextQuestion(question: question)
+            }
+        }
     }
     
     func setup(delegate: QuestionFactoryDelegate) {
